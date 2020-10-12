62027dcb401fc35e8672b6b789ab7a6b4ed3a259
nichemarket <riittiir@gmail.com>
2020-10-12 09:04:58 -0500

    🚀 Initial Commit

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 var mountNode = document.getElementById("app");
-ReactDOM.render(<h1>Wow, this is much smaller than CRA!!! 🔥</h1>, mountNode);
+ReactDOM.render(<h1>Niche!</h1>, mountNode);
